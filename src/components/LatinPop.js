be648e06030c6a6d85da7b612cbe7cff21bfa3c9
jaymalave <jaym.github@gmail.com>
2022-02-14 21:59:42 +0530

    add fade effect to gallery images

--- src/components/LatinPop.js
+++ src/components/LatinPop.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import { CSSTransition, SwitchTransition } from 'react-transition-group'
+import './LatinPop.css';
 
 const LatinPop = () => {
   const trendingEvents = [
     { id: 1, name: 'img1', img_url: '/assets/gallery.png' },
-    { id: 2, name: 'img2', img_url: '/assets/twitter.png'},
-    { id: 3, name: 'img3', img_url: '/assets/youtube.png'},
+    { id: 2, name: 'img2', img_url: '/assets/gallery.png'},
+    { id: 3, name: 'img3', img_url: '/assets/gallery.png'},
   ]
   const [principalImage, setPrincipalImage] = useState(Array.isArray(trendingEvents) && trendingEvents.length > 0 ? trendingEvents[0] : null)
   const [inImage, setInImage] = useState(true)
@@ -44,60 +45,12 @@ const LatinPop = () => {
           in={inImage}
           appear
           timeout={{ enter: 500, exit: 2000 }}
-          classNames="fade"
+          classNames="my-node"
         >
           <img alt={principalImage.name} src={principalImage.img_url} />
         </CSSTransition>
       </div>
 
-      <style jsx>{`
-        @import "styles/variables";
-        @import "styles/mixins";
-        
-        .principal-section {
-          .principal-image-container {
-            z-index: 10;
-
-            img {
-              height: 25em;
-
-              @include media-breakpoint-down(md) {
-                height: 15em;
-              }
-            }
-
-            .fade-appear {
-              opacity: 0.01;
-            }
-
-            .fade-appear.fade-appear-active {
-                opacity: 1;
-                transition: opacity 500ms ease-in;
-            }
-
-            .fade-enter {
-              opacity: 0.01;
-            }
-
-            .fade-enter.fade-enter-active {
-                opacity: 1;
-                transition: opacity 500ms ease-in;
-            }
-
-            /* exit */
-            .fade-exit {
-                opacity: 1;
-            }
-            .fade-exit.fade-exit-active {
-                opacity: 0.01;
-                transition: opacity 2000ms ease-in;
-            }
-            .fade-exit-done {
-              opacity: 0.01;
-            }
-          }
-        }
-      `}</style>
     </div>
   )
 }
