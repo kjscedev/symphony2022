773eae81dba4de9ecefabce3e040ad311f67092d
jaymalave <jaym.github@gmail.com>
2022-02-24 11:51:48 +0530

    let user know about the game

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -62,7 +62,7 @@ const Home = (props) => {
         <div className="col game-col">
           <div className="gamecontainer">
             <div>
-              {/* <p style={{ color: "red", fontWeight: "bold" }}>High Score:</p> */}
+              <p style={{ color: "black", fontWeight: "bold" }}>Press Space bar to start playing the game!</p>
             </div>
           </div>
 
